089ead96facbec13692dcbfb18ced1eff846bade
briandridge <briandridge@gmail.com>
2017-04-19 21:20:04 -0600

    finished! adds ballons, makes alphabet animations work

--- js/app.js
+++ js/app.js
@@ -13,29 +13,30 @@ $(".redrocks").hide();
 			// $(this).html(letter);
 			$(this).replaceWith('<span>' + letter + '</span>');
 			$(this).removeClass('correctguess').addClass("correctguess-active animated fadeInUp");
-			console.log(this);
+			// console.log(this);
 		});
 	}
 
 // pushes guessed letters into correct and incorrect arrays
-	// function pushLetters(letter) {
-	// 	for (var i = 0; i < winArray.length; i++) {
-	// 		if  (winArray[i] === letter) {
-	// 			correctGuessArray.push(letter);
-	// 		}
-	// 		else {
-	// 			wrongGuessArray.push(letter);
-	// 		}
-	// 		console.log("correct" + correctGuessArray);
-	// 		console.log("wrong" + correctGuessArray);
-	// 	}
-	// }
+	function pushLetters(letter) {
+		for (var i = 0; i < winArray.length; i++) {
+			if  (winArray[i] === letter) {
+				correctGuessArray.push(letter);
+			}
+			// else {
+			// 	wrongGuessArray.push(letter);
+			// }
+			// console.log("correct" + correctGuessArray);
+			// console.log("wrong" + wrongGuessArray);
+		}
+	}
 
 // plays song and shows Red Rocks div if won, enables pause with space bar
 	function checkWin() {
 		if(correctGuessArray.length === 8) {
 			audio.play();
 			$(".redrocks").show();
+			$('.balloon').show();
 			$(".placeholder").hide();
 		}
 		$(document).keydown(function(e) {
@@ -46,60 +47,41 @@ $(".redrocks").hide();
 	}
 
 // works
-// click listener on letters: changes styling and fires all other functions
-	// $('.alpha').one("click",function() {
-	// 	$(this).removeClass('unclicked');
-	// 	$(this).addClass('clicked animated');
-	// 	// $(this).addClass('animated bounceInUp');
-	// 	correctGuess(this.id);
-	// 	pushLetters(this.id);
-	// 	checkWin();
-	// 	console.log(this.id);		
-	// });
+	$('.alphaCorrect').one("click",function() {
+		$(this).removeClass('unclicked').addClass('clicked animated bounce');
+		correctGuess(this.id);
+		pushLetters(this.id);
+		checkWin();	
+	});
+
+	$('.alphaWrong').one("click",function() {
+		$(this).removeClass('unclicked').addClass('clicked animated shake');
+		correctGuess(this.id);
+		pushLetters(this.id);
+		checkWin();	
+	});
+
 
 // in progress
-	$('.alpha').one("click",function() {
-		console.log(this.id);
-		if (this.id === "A" || "E" || "C" || "H" || "N" || "P" || "R" || "T" ) {
-			$(this).removeClass('unclicked').addClass('clicked animated bounce');
-			correctGuessArray.push(this.id);
-			console.log("correct " + correctGuessArray);
-
-		}
-		else {
-			$(this).removeClass('unclicked').addClass('clicked animated shake');
-			// wrongGuessArray.push(this.id);
-			// console.log("wrong " + wrongGuessArray);
-		}
-		correctGuess(this.id);
-		// pushLetters(this.id);
-		checkWin();
-		// console.log(this.id);		
-	});
-});
-
-
-// doesn't work
 	// $('.alpha').one("click",function() {
-	// 	// var clicked = $(this);
-	// 	var letter = $(this.id);
-	// 	console.log(letter);
+	// 	console.log(this.id);
+	// 	if (this.id === ("A" || "E" || "C" || "H" || "N" || "P" || "R" || "T") ) {
+	// 		console.log(this.id === "A");
 
-	// 	for (var i = 0; i < winArray.length; i++) {
-	// 		if  (winArray[i] === letter) {
-	// 			console.log(letter);
-	// 			correctGuessArray.push(letter);
-	// 			correctGuess();
-	// 			checkWin();
-	// 			$(this).removeClass('unclicked');
-	// 			$(this).addClass('clicked animated bounceInUp');
-	// 			console.log(correctGuessArray);
-	// 		}
-	// 		else {
-	// 			wrongGuessArray.push(letter);
-	// 			$(this).removeClass('unclicked');
-	// 			$(this).addClass('clicked animated shake');
-	// 		}
+	// 		$(this).removeClass('unclicked').addClass('clicked animated bounce');
+	// 		correctGuessArray.push(this.id);
+	// 		console.log("correct " + correctGuessArray);
+
 	// 	}
+	// 	else {
+	// 		$(this).removeClass('unclicked').addClass('clicked animated shake');
+	// 		// wrongGuessArray.push(this.id);
+	// 		// console.log("wrong " + wrongGuessArray);
+	// 	}
+	// 	correctGuess(this.id);
+	// 	// pushLetters(this.id);
+	// 	checkWin();
+	// 	// console.log(this.id);		
 	// });
 // });
+});
